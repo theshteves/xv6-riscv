a5c46cd40d0a3188876387f9833cb794c1a9e878
theshteves <stevenkneiser@gmail.com>
2024-11-18 03:14:27 -0500

    Have fun with new `wish` prompt & cleaner main()

--- user/wish.c
+++ user/wish.c
@@ -1,5 +1,6 @@
 // Shell.
 
+#include <stdbool.h>
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
@@ -13,8 +14,10 @@
 
 #define MAX_ARGC 10
 #define MAX_LINE_LENGTH 256
-#define PROMPT_STRING "wish> "
-#define PROMPT_STRING_LENGTH 6
+//#define PROMPT_STRING "wish> "
+#define SAD_PROMPT   "\033[31m0_0\033[0m "
+#define HAPPY_PROMPT "\033[33m^_^\033[0m "
+#define PROMPT_STRING_LENGTH 32
 
 
 size_t strspn(const char *str, const char *delim) {
@@ -79,63 +82,73 @@ char *strtok_r(char *str, const char *delim, char **saveptr) {
 }
 
 
-int eval(int argc, char* argv[MAX_ARGC]) {
+bool eval(int argc, char* argv[MAX_ARGC], char* path) {
     if (argv[0] == NULL) {
-        return 0;
+    } else if (strcmp(argv[0], "help") == 0) {
+        printf("nope.\n");
 
     } else if (strcmp(argv[0], "exit") == 0) {
         exit(0);
 
-    } else if (strcmp(argv[0], "help") == 0) {
-        printf("nope.\n");
+    } else if (strcmp(argv[0], "cd") == 0) {
+        chdir(argv[1]);
 
     } else {
         int pid = fork();
         if (pid < 0) {
-            fprintf(2, "fork failed\n");
-            exit(2);
+            fprintf(2, "ERROR: fork failed\n");
+            return false;
+
         } else if (pid == 0) {  // Child process
             exec(argv[0], argv);  // Execute the command
             fprintf(2, "ERROR: Command failed: `%s`\n", argv[0]);  // Should never get here if exec works
-            exit(2);  // Exit child process if exec fails
+            return false;  // Exit child process if exec fails
+
         } else {  // Parent process
             int time = 5000;
             wait(&time);  // Wait for the child process to finish
         }
-
     }
     //TODO: printf("Command not found: `%s`\n", argv[0]);
-    return 0;
+    return true;
 }
 
 
-int main(int argc, char **argv) {
+int main() {
     char buf[MAX_LINE_LENGTH];
-    int _argc;
-    char* _argv[MAX_ARGC];
-    char *placeholder;
-    char *token;
+    int argc;
+    char* argv[MAX_ARGC];
+    char* placeholder;
+    char* token;
+    bool is_happy = true;
+    char* path = ".";
 
     while (1) {
-        _argc = 0;
-        memset(_argv, 0, sizeof(_argv));
-        printf(PROMPT_STRING);
+        argc = 0;
+        memset(argv, 0, sizeof(argv));
+        printf("%d ", uptime() / 10);
+        printf(is_happy ? HAPPY_PROMPT : SAD_PROMPT);
         gets(buf, MAX_LINE_LENGTH);
 
         // Trim trailing newline
         size_t len = strlen(buf);
-        if (len > 0 && buf[len - 1] == '\n') {
-            buf[len - 1] = '\0';
+        if (len > 0) {
+            if (buf[len - 1] == '\n') {
+                buf[len - 1] = '\0';
+
+            } else if (buf[len - 1] == -1) {
+                exit(0);
+            }
         }
 
         // tokenize input
         token = strtok_r(buf, " ", &placeholder);
         while (token != NULL) {
-            _argv[_argc++] = token;
+            argv[argc++] = token;
             token = strtok_r(NULL, " ", &placeholder);
         }
 
-        if (eval(_argc, _argv) != 0) { break; }
+        is_happy = eval(argc, argv, path);
     }
 
     return 0;
